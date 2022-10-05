fc57c40f1ab5cd74810b0dda639ef517a020d0bd
mavrk-mose <kippemoses25@gmail.com>
2022-10-06 00:35:41 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="home">
+      <div className="navbar">
+        
+      </div>
+      <div className="content">
+
+      </div>
+      <div className="filters">
+
+      </div>
+      <div className="cards">
+        <div className="card">
+            <div className="container">
+              <p>Dar Es Salaam</p>
+              <ul>windspeed</ul>
+              <ul>Tuesday : 19:52</ul>
+              <ul>Cloudy</ul>
+            </div>
+        </div>
+        <div className="card">
+          <div className="container">
+              <p>Arusha</p>
+              <ul>windspeed</ul>
+              <ul>Tuesday : 19:52</ul>
+              <ul>Cloudy</ul>
+          </div>
+        </div>
+        <div className="card">
+          <div className="container">
+              <p>Njombe</p>
+              <ul>windspeed</ul>
+              <ul>Tuesday : 19:52</ul>
+              <ul>Cloudy</ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
