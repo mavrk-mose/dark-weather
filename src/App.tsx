9d23d4310f422b9e395e8d2f44bdde8cc65af455
mavrk-mose <kippemoses25@gmail.com>
2022-10-07 15:20:31 +0300

    rendered API data for each weather card

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,13 @@ import moment, { HTML5_FMT } from 'moment';
 function App() {
 
   const [data, setData] = useState([]);
+  const [data1, setData1] = useState([]);
+  const [data2, setData2] = useState([]);
 
+//fetch data for Dar Es Salaam
   useEffect(() => {
     const fetchData =async function (){
-      await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Arusha&appid=93de778a8de80994ecaaee49126e92e9&units=metric`)
+      await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Dar Es Salaam&appid=93de778a8de80994ecaaee49126e92e9&units=metric`)
       .then(res => res.json())
       .then(result => {
         setData(result)
@@ -19,7 +22,30 @@ function App() {
     } 
     fetchData();
   }, []);
-
+//fetch data for Arusha
+  useEffect(() => {
+    const fetchData =async function (){
+      await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Arusha&appid=93de778a8de80994ecaaee49126e92e9&units=metric`)
+      .then(res => res.json())
+      .then(result => {
+        setData1(result)
+        console.log(result);
+      });
+    } 
+    fetchData();
+  }, []);
+//fetch data for Njombe
+  useEffect(() => {
+    const fetchData =async function (){
+      await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Njombe&appid=93de778a8de80994ecaaee49126e92e9&units=metric`)
+      .then(res => res.json())
+      .then(result => {
+        setData2(result)
+        console.log(result);
+      });
+    } 
+    fetchData();
+  }, []);
   return (
   <div className="container">
     <div className="app">
@@ -63,24 +89,24 @@ function App() {
         <img src={sunny} className="weather-icon-2" alt="sunny" />
           <div className="info-card">
           <div className="name-temp">
-              <h4>{data.name}</h4>
-              {data.main ? <h5>{data.main.temp}ºC</h5> : null}
+              <h4>{data1.name}</h4>
+              {data1.main ? <h5>{data1.main.temp}ºC</h5> : null}
               </div>
-              {data.wind ? <ul>windspeed : {data.wind.speed}km/h</ul> : null}
+              {data1.wind ? <ul>windspeed : {data1.wind.speed}km/h</ul> : null}
               <ul>{moment().format('dddd')}: {moment().format('HH:mm')}</ul>
-              {data.weather ? <ul>{data.weather[0].main}</ul> : null}     
+              {data1.weather ? <ul>{data1.weather[0].main}</ul> : null}     
           </div>
         </div>
         <div className="card">
         <img src={rainy} className="weather-icon-3" alt="rainy" />
           <div className="info-card">
           <div className="name-temp">
-              <h4>{data.name}</h4>
-              {data.main ? <h5>{data.main.temp}ºC</h5> : null}
+              <h4>{data2.name}</h4>
+              {data2.main ? <h5>{data2.main.temp}ºC</h5> : null}
               </div>
-              {data.wind ? <ul>windspeed : {data.wind.speed}km/h</ul> : null}
+              {data2.wind ? <ul>windspeed : {data2.wind.speed}km/h</ul> : null}
               <ul>{moment().format('dddd')}: {moment().format('HH:mm')}</ul>
-              {data.weather ? <ul>{data.weather[0].main}</ul> : null}
+              {data2.weather ? <ul>{data2.weather[0].main}</ul> : null}
           </div>
         </div>
       </div>
